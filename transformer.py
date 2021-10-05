6e80699a0f870060584a4d0feff714017eb50157
Ritik Mathur <53309724+RitikMathur04@users.noreply.github.com>
2021-10-05 23:38:12 +0530

    Create transformer.py

---
+++ transformer.py
@@ -0,0 +1,12 @@
+from transformers import AutoTokenizer, AutoModel
+
+tokenizer = AutoTokenizer.from_pretrained("nlpaueb/legal-bert-base-uncased")
+model = AutoModel.from_pretrained("nlpaueb/legal-bert-base-uncased")
+tokenized = batch_1[0].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))
+max_len = 0
+for i in tokenized.values:
+    if len(i) > max_len:
+        max_len = len(i)
+
+padded = np.array([i + [0]*(max_len-len(i)) for i in tokenized.values])
+np.array(padded).shape
